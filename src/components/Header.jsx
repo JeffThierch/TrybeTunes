346355806a57d3dbe4580788ce1e8cdd3a287e32
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-11-11 18:48:58 -0300

    :sparkles: Feat: Implement Nav bar With Links
    git push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 export default class Header extends Component {
@@ -34,6 +35,11 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <h1>TrybeTunes</h1>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
         <p data-testid="header-user-name">{userName}</p>
       </header>
     );
