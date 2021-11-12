758c163341e0a401549085178c3d92bdce78cc69
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-11-12 11:40:18 -0300

    :sparkles: Feat: Implement Search Input and validation

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,57 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Input from '../components/Input';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchInput: '',
+      disabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchInput: value,
+    }, () => (this.verifyInput()));
+  }
+
+  verifyInput = () => {
+    const { searchInput } = this.state;
+    if (searchInput.length >= 2) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <input
+            type="text"
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+            placeholder="Nome do Artista"
+          />
+          <button
+            data-testid="search-artist-button"
+            disabled={ disabled }
+            type="button"
+          >
+            Pesquisar
+
+          </button>
+        </div>
       </>
     );
   }
