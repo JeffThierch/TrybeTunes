92d524a99678ffc6e0f8170c4794decafae7253f
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-11-16 11:03:02 -0300

    :sparkles: Feat: Render All Fav Music in Favorites Area
    Create the logic to render all saved music in Favorites Ares

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <div data-testid="page-favorites" />
-      </>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      musics: [],
+      loading: false,
+    };
   }
+
+  componentDidMount() {
+    this.recoverMusicsFromLocalStorage();
+  }
+
+  recoverMusicsFromLocalStorage = () => {
+    this.setState({ loading: true }, async () => {
+      const favMusics = await getFavoriteSongs();
+      this.setState({
+        musics: favMusics,
+        loading: false,
+      });
+    });
+  }
+
+   handleChange = async (music) => {
+     const { musics } = this.state;
+     let favArray = [...musics];
+     const haveId = favArray.some(({ trackId }) => trackId === music.trackId);
+     this.setState({ loading: true });
+
+     if (haveId) {
+       await removeSong(music);
+       favArray = favArray.filter(({ trackId }) => trackId !== music.trackId);
+     }
+     this.setState({ loading: false, musics: favArray });
+   }
+
+   render() {
+     const { musics, loading } = this.state;
+     return (
+       <>
+         <Header />
+         {loading ? <p>Carregando...</p> : (
+           <div data-testid="page-favorites">
+             {musics.map((music) => (
+               <MusicCard
+                 key={ music.trackId }
+                 musicList={ music }
+                 isChecked={ musics.some(
+                   ({ trackId }) => trackId === music.trackId,
+                 ) }
+                 handleChange={ () => this.handleChange(music) }
+               />
+             ))}
+           </div>
+         )}
+       </>
+     );
+   }
 }
 
 export default Favorites;
